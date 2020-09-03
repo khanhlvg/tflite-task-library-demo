8b6c870f40a4a62932211c6baa1208a2981cb2db
Khanh LeViet <khanhlvg@google.com>
2020-09-03 22:47:11 +0900

    Minor update to ImageSegmenter demo

--- app/src/main/java/com/khanhlvg/taskapidemo/ImageSegmenterActivity.java
+++ app/src/main/java/com/khanhlvg/taskapidemo/ImageSegmenterActivity.java
@@ -4,14 +4,11 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.util.Log;
-import android.view.View;
 import android.widget.ImageView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import java.io.IOException;
-import java.nio.ByteBuffer;
 import java.util.List;
-import org.checkerframework.checker.nullness.qual.NonNull;
 import org.tensorflow.lite.support.image.TensorImage;
 import org.tensorflow.lite.task.vision.segmenter.ColoredLabel;
 import org.tensorflow.lite.task.vision.segmenter.ImageSegmenter;
@@ -23,6 +20,8 @@ public class ImageSegmenterActivity extends AppCompatActivity {
 
   private TensorImage tensorImage;
   private ImageSegmenter imageSegmenter;
+  private ImageView originalImageView;
+  private ImageView outputImageView;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -31,22 +30,23 @@ public class ImageSegmenterActivity extends AppCompatActivity {
 
     Bitmap image = BitmapFactory.decodeResource(getResources(), R.drawable.girl);
     tensorImage = TensorImage.fromBitmap(image);
-    ImageView originalImageView = findViewById(R.id.original_bitmap);
+    originalImageView = findViewById(R.id.original_bitmap);
     originalImageView.setImageBitmap(image);
 
-    // Initialization
-    ImageSegmenterOptions options = ImageSegmenterOptions.builder().setOutputType(OutputType.CATEGORY_MASK).build();
     try {
+      // Initialization
+      ImageSegmenterOptions options = ImageSegmenterOptions.builder().setOutputType(OutputType.CATEGORY_MASK).build();
       imageSegmenter = ImageSegmenter.createFromFileAndOptions(this, "deeplabv3.tflite", options);
 
       // Run inference
       List<Segmentation> results = imageSegmenter.segment(tensorImage);
       Segmentation result = results.get(0);
-      List<ColoredLabel> coloredLabels = result.getColoredLabels();
       TensorImage tensorMask = result.getMasks().get(0);
-
       int[] rawMask = tensorMask.getTensorBuffer().getIntArray();
+
+      // Convert from raw mask data into a Bitmap instance
       int[] pixelData = new int[rawMask.length * 3];
+      List<ColoredLabel> coloredLabels = result.getColoredLabels();
       for (int i=0;i<rawMask.length; i++) {
         int color = coloredLabels.get(rawMask[i]).getColor().toArgb();
         pixelData[3 * i] = Color.red(color);
@@ -56,10 +56,12 @@ public class ImageSegmenterActivity extends AppCompatActivity {
       int[] shape = {tensorMask.getWidth(), tensorMask.getHeight(), 3};
       TensorImage maskImage = new TensorImage();
       maskImage.load(pixelData, shape);
-      ImageView imageView = findViewById(R.id.segmentation_bitmap);
-      imageView.setImageBitmap(maskImage.getBitmap());
+
+      // Show the segmentation visualization on screen
+      outputImageView = findViewById(R.id.segmentation_bitmap);
+      outputImageView.setImageBitmap(maskImage.getBitmap());
     } catch (IOException e) {
-      Log.e("ImageSegmenter", "Error:", e);
+      Log.e("ImageSegmenter", "Error: ", e);
     }
   }
 }
